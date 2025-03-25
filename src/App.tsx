338afad947167e20ec33bba03733b566488cbca3
OlhaOliinyk <olaolijnik71@gmail.com>
2025-03-25 19:17:42 +0100

    added Homework 6

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,21 @@
 // импорт компонента Lesson06 по умолчанию
 // при импорте по умолчанию можно использовать другое названия для компонента
 // import Task6 from './lessons/Lesson06/Lesson06';
-import Lesson06 from './lessons/Lesson06/Lesson06';
+// import Homework06 from './homeworks/HomeWork06';
+// import Lesson06 from './lessons/Lesson06/Lesson06';
+import Homework06 from "./homeworks/HomeWork06";
 
 function App() {
 
   return (
     <>
+    <Homework06 />
       {/* <Task6 /> */}
-      <Lesson06 />
+      {/* <Lesson06 /> */}
       {/* <Homework06/> */}
     </>
-  )
+  );
 }
 
-export default App
+export default App;
 
---
+++ src/components/Input.tsx
@@ -0,0 +1,17 @@
+import './Button/styles.css';
+
+function Input() {
+    return (
+        <>
+        <h3 mp-1>Name:</h3>
+        <input className="input" type="text" placeholder="Please enter your name" />
+        </>
+    ) ;
+  }
+  
+  export default Input;
+
+
+
+
+
