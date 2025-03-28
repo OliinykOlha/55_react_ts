ac2b1d5e202eab715d736aef337819946ecfa999
OlhaOliinyk <olaolijnik71@gmail.com>
2025-03-28 10:39:22 +0100

    fixed Homewor08 , input password, button

--- src/homeworks/HomeWork08/HomeWork08.tsx
+++ src/homeworks/HomeWork08/HomeWork08.tsx
@@ -2,37 +2,35 @@ import { ChangeEvent, useState } from "react";
 import Button from "../../components/Button/Button";
 import Input from "../../components/Input/Input";
 import './styles.css'
+function Homework08() {
+  const [passwordValue, setPasswordValue] = useState<string>('');
+  const [isShowPassword, setIsShowPassword] = useState<boolean>(false);
+  const onChangePassword = (event: ChangeEvent<HTMLInputElement>) => {
+    setPasswordValue(event.target.value)
+  }
+  const showPassword = () => {
+    setIsShowPassword(true)
+  }
+  const hidePassword = () => {
+    setIsShowPassword(false)
+  }
 
-function HomeWork08 () {
-
- const [password, setPassword] = useState<string>('');
-const onChangePassword = (event: ChangeEvent<HTMLInputElement>) => {
-    setPassword(event.target.value)
+  return (
+    <div className="homework08-container">
+      <h1>Change password</h1>
+      <Input
+        name='password'
+        label='Password'
+        id='password_id'
+        placeholder="Enter your password"
+        type='password'
+        value={passwordValue}
+        onChange={onChangePassword}
+      />
+      <Button name='SHOW PASSWORD' onClick={showPassword} />
+      <Button name='HIDE PASSWORD' onClick={hidePassword} />
+      {isShowPassword && <div className="result-container">{passwordValue}</div>}
+    </div>
+  )
 }
-
-const [showPassword, setShowPassword ] = useState<boolean>(false);
-const showPasswordHandler = () => {
-    setShowPassword(!showPassword)
-}
-
-
-
-    return (
-        <div className="password-card-container">
-            <h2>Change password</h2>
-            <label>Password</label>
-            <Input 
-            name="inputPassword"
-            placeholder="Enter your password"
-            type= "password"
-            value={password}
-            onChange={onChangePassword} />
-            <Button name="Show Password" onClick={showPasswordHandler}/>
-            {showPassword && <div>{password}</div>}
-            
-        </div>
-    )
-}
-
-
-export default HomeWork08;
+export default Homework08;
