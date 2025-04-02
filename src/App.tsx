455b025068871f0323ed11b76a7b5a291679ca82
OlhaOliinyk <olaolijnik71@gmail.com>
2025-04-02 18:25:35 +0200

    added Homework12

--- src/App.tsx
+++ src/App.tsx
@@ -2,10 +2,11 @@ import GlobalStyles from "./styles/GlobalStyles"
 
 //Lessons imports
 // import Lesson11 from "./lessons/Lesson11/Lesson11"
-import Lesson12 from "./lessons/Lesson12/Lesson12"
+// import Lesson12 from "./lessons/Lesson12/Lesson12"
 
 //Homeworks imports
 // import HomeWork11 from "./homeworks/HomeWork11/HomeWork11"
+import HomeWork12 from "./homeworks/HomeWork10/HomeWork12/HomeWork12"
 
 
 function App () {
@@ -14,7 +15,8 @@ function App () {
     <>
       <GlobalStyles />
       {/* <HomeWork11 /> */}
-      <Lesson12 />
+      {/* <Lesson12 /> */}
+      <HomeWork12 />
       </>
     
   )
---
+++ src/homeworks/HomeWork10/HomeWork12/styles.ts
@@ -0,0 +1,31 @@
+import styled from "@emotion/styled";
+
+export const HomeWork12Container = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 30px;
+  flex: 1;
+  padding: 60px;
+  background-color: rgb(220, 217, 179);
+`;
+
+export const CatsFactsWrapper = styled.div`
+display: flex;
+ flex-direction: column;
+ align-items: center;
+ justify-content: center;
+ min-width: 500px;
+ min-height: 400px;
+ padding: 30px;
+ background-color: white;
+ border-radius: 10px;
+ font-size: 24px;
+ color:rgb(220, 217, 179);
+ font-weight: bold;
+`
+export const Error = styled.div`
+color: red;
+font-size: 25;
+font-weight: bold;
+`
