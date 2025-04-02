32407d694b8b618381d832635ab02d67c867aaaf
OlhaOliinyk <olaolijnik71@gmail.com>
2025-04-02 13:07:14 +0200

    added Lesson 12, axios

--- src/App.tsx
+++ src/App.tsx
@@ -1,50 +1,20 @@
 import GlobalStyles from "./styles/GlobalStyles"
 
 //Lessons imports
-// импорт компонента Lesson06 по умолчанию
-// при импорте по умолчанию можно использовать другое названия для компонента
-// import Task6 from './lessons/Lesson06/Lesson06';
-// import Lesson06 from './lessons/Lesson06/Lesson06';
-// import Lesson07 from "./lessons/Lesson07/Lesson07"
-// import Lesson08 from "./lessons/Lesson08/Lesson08"
-// import Lesson06 from "./lessons/Lesson06/Lesson06"
-// import Lesson09 from "./lessons/Lesson09/Lesson09"
-// import Lesson10 from "./lessons/Lesson10/Lesson10"
 // import Lesson11 from "./lessons/Lesson11/Lesson11"
+import Lesson12 from "./lessons/Lesson12/Lesson12"
 
 //Homeworks imports
-// import Homework06 from "./homeworks/Homework06/Homework06"
-// import Homework07 from "./homeworks/Homework07/Homework07"
-// import Homework08 from "./homeworks/Homework08/Homework08"
-// import Homework09 from "./homeworks/Homework09/Homework09"
-// import HomeWork10 from "./homeworks/HomeWork10/HomeWork10"
-import HomeWork11 from "./homeworks/HomeWork11/HomeWork11"
+// import HomeWork11 from "./homeworks/HomeWork11/HomeWork11"
 
-
-//Consultations imports
-// import Consultation04 from "./consultations/Consultation04/Consultation04"
-// import Consultation05 from "./consultations/Consultation05/Consultation05"
 
 function App () {
 
   return (
     <>
       <GlobalStyles />
-      {/* <Task6 /> */}
-      {/* <Lesson06 /> */}
-      {/* <Homework06 /> */}
-      {/* <Lesson07 /> */}
-      {/* <Homework07/> */}
-      {/* <Lesson08/> */}
-      {/* <Consultation04 /> */}
-      {/* <Homework08 /> */}
-      {/* <Lesson09 /> */}
-      {/* <Homework09 /> */}
-      {/* <Lesson10 /> */}
-      {/* <HomeWork10 /> */}
-      {/* <Lesson11 /> */}
-      {/* <Consultation05/> */}
-      <HomeWork11 />
+      {/* <HomeWork11 /> */}
+      <Lesson12 />
       </>
     
   )
