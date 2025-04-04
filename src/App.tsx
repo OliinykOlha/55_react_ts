a9d3136c854a5edce29e202e11e5c73dbc0f48f0
OlhaOliinyk <olaolijnik71@gmail.com>
2025-04-04 08:01:59 +0200

    added consultation06

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,12 @@ import GlobalStyles from "./styles/GlobalStyles"
 //Lessons imports
 // import Lesson11 from "./lessons/Lesson11/Lesson11"
 // import Lesson12 from "./lessons/Lesson12/Lesson12"
- import Lesson13 from "./lessons/Lesson13/Lesson13"
+//  import Lesson13 from "./lessons/Lesson13/Lesson13"
 
 //Homeworks imports
 // import HomeWork11 from "./homeworks/HomeWork11/HomeWork11"
 //  import HomeWork12 from "./homeworks/HomeWork10/HomeWork12/HomeWork12"
+import Consultation06 from "./consultations/Consultation06/Consultation06"
 
 
 function App () {
@@ -18,7 +19,8 @@ function App () {
       {/* <HomeWork11 /> */}
       {/* <Lesson12 /> */}
       {/* <HomeWork12 /> */}
-      <Lesson13 />
+      {/* <Lesson13 /> */}
+      <Consultation06 />
       </>
     
   )
