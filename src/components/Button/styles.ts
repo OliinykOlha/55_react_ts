9ec42fc7b2a7fec79053f99396e9586e9a269188
OlhaOliinyk <olaolijnik71@gmail.com>
2025-04-05 11:52:47 +0200

    lesson_13_practice

--- src/components/Button/styles.ts
+++ src/components/Button/styles.ts
@@ -5,17 +5,18 @@ interface MainButtonStyledProps {
 }
 
 export const MainButton = styled.button<MainButtonStyledProps>`
-  width: 100%;
+  /* width: 100%; */
+  width: 200px;
   height: fit-content;
   padding: 20px;
   background: ${({ isDanger }) =>
     isDanger ? "rgb(193, 7, 7)" : "rgb(17, 48, 127)"};
   border: none;
   border-radius: 6px;
-  font-size: 26px;
+  font-size: 14px;
   color: white;
   cursor: pointer;
   &:disabled {
-    background-color: rgb(202, 200, 200);
+  background-color: rgb(202, 200, 200);
   }
 `;
